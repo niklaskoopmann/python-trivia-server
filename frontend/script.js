8f4360ffff3590ab571c163712ab68b25b780e59
niklaskoopmann <koopmann.niklas@gmail.com>
2020-05-10 12:23:22 +0200

    fixed pie chart

--- frontend/script.js
+++ frontend/script.js
@@ -8,18 +8,28 @@ function populateDropdowns() {
   });
 }
 
-var ctxP = document.getElementById("pieChart").getContext('2d');
-var myPieChart = new Chart(ctxP, {
-  type: 'pie',
-  data: {
-    labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-    datasets: [{
-      data: [300, 50, 100, 40, 120],
-      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-      hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-    }]
-  },
-  options: {
-    responsive: true
-  }
-});
+function drawChart(){
+  var ctxP = document.getElementById("chart-area").getContext('2d');
+  var myPieChart = new Chart(ctxP, {
+    type: 'pie',
+    data: {
+      labels: ["Brown", "Blue", "Orange", "Yellow", "Pink", "Green"],
+      datasets: [{
+        data: [1, 1, 1, 1, 1, 1],
+        backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360", "#000000"],
+        //onClick: [function(){console.log("a")}, function(){console.log("b")}, function(){console.log("c")}, function(){console.log("d")}, function(){console.log("e")}, function(){console.log("f")}]
+        //hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774", "#000000"]
+      }]
+    },
+    options: {
+      responsive: true,
+      tooltips: {
+        mode: ''
+      },
+      legend: {
+        position: 'bottom',
+        onClick: null
+      }
+    }
+  });
+}
