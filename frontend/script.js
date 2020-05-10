031e0a6710fc76f0ff78c59c6fa62370d2098046
Chiara Kramer <cs.kramer@hotmail.de>
2020-05-10 11:57:35 +0200

    tried to add pie chart

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-function populateDropdowns () {
+function populateDropdowns() {
   var categories;
   $.get("http://192.168.179.69:42069/about/categories", function(data, status) {
     console.log(data);
@@ -7,3 +7,19 @@ function populateDropdowns () {
     });
   });
 }
+
+var ctxP = document.getElementById("pieChart").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+  type: 'pie',
+  data: {
+    labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+    datasets: [{
+      data: [300, 50, 100, 40, 120],
+      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+      hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+    }]
+  },
+  options: {
+    responsive: true
+  }
+});
