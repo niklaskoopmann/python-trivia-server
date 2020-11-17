3639625da64efd62c7972366730abe4538b70abc
niklaskoopmann <koopmann.niklas@gmail.com>
2020-11-17 22:26:48 +0100

    fixed SQL and insert script

--- database/insert_values.py
+++ database/insert_values.py
@@ -3,21 +3,26 @@ import os
 import csv
 
 trivia_database = mysql.connector.connect(
-    host="172.17.0.2",
+    host="192.168.2.100",
     user="root",
     passwd="123456",
-    database="trivia_questions"
+    database="trivia_questions",
+    auth_plugin="mysql_native_password"
 )
+
+current_script_directory = os.path.dirname(os.path.realpath(__file__))
 
 print(trivia_database)
 
-with os.scandir("../questions/") as files:
+with os.scandir(current_script_directory + "/../questions/") as files:
     for questions_file in files:
         print(questions_file.name)
         index = 0
-        with open("../questions/" + questions_file.name, "rt", encoding="utf-8") as csvfile:
+        with open(current_script_directory + "/../questions/" + questions_file.name, "rt", encoding="utf-8") as csvfile:
             reader = csv.reader(csvfile, delimiter=";")
             for row in reader:
+                print(row)
+                print("INSERT INTO " + os.path.splitext(questions_file.name)[0] + " VALUES (%s, %s, %s)", (index, row[0], row[1]))
                 trivia_database.cursor().execute("INSERT INTO " + os.path.splitext(questions_file.name)[0] + " VALUES (%s, %s, %s)", (index, row[0], row[1]))
                 #trivia_database.cursor().execute("INSERT INTO " + os.path.splitext(questions_file.name)[0] + " VALUES (" + str(index) + ", \'" + row[0] + "\', \'" + row[1] + "\')")
                 index += 1
--- database/setup.sql
+++ database/setup.sql
@@ -1,5 +1,5 @@
 create database trivia_questions;
-select trivia_questions;
+use trivia_questions;
 
 create table art_literature (id INT, question TEXT, answer TINYTEXT);
 create table entertainment (id INT, question TEXT, answer TINYTEXT);
@@ -7,12 +7,12 @@ create table food_drink (id INT, question TEXT, answer TINYTEXT);
 create table general (id INT, question TEXT, answer TINYTEXT);
 create table geography (id INT, question TEXT, answer TINYTEXT);
 create table history_holidays (id INT, question TEXT, answer TINYTEXT);
-create table language (id INT, question TEXT, answer TINYTEXT
+create table language (id INT, question TEXT, answer TINYTEXT);
 create table mathematics (id INT, question TEXT, answer TINYTEXT);
 create table music (id INT, question TEXT, answer TINYTEXT);
 create table people_places (id INT, question TEXT, answer TINYTEXT);
 create table religion_mythology (id INT, question TEXT, answer TINYTEXT);
-create table science_nature (id INT, question TEXT, answer TINYTEXT
+create table science_nature (id INT, question TEXT, answer TINYTEXT);
 create table sports_leisure (id INT, question TEXT, answer TINYTEXT);
 create table technology_video_games (id INT, question TEXT, answer TINYTEXT);
 create table toys_games (id INT, question TEXT, answer TINYTEXT);
